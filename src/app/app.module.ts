457658769166ffe9a98b0d84ff05c9fc19544889
NickChlam <Nick.Chlam@rht.com>
2017-08-18 07:57:36 -0600

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServerComponent} from './server/server.component'
 
 import { AppComponent } from './app.component';
 
@@ -7,6 +8,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [
     AppComponent,
+    ServerComponent
     
   ],
   imports: [
